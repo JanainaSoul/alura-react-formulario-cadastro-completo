6f4d99eaee137d79c2c717a1993fd7f21b9333a2
Janaina <janainasoul@icloud.com>
2023-05-16 18:13:41 -0300

    Injeção de dependências

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,13 +1,17 @@
 import { TextField , Button} from '@material-ui/core';
 import React from 'react';
 
-function DadosUsuario() {
+function DadosUsuario({aoEnviar}) {
     return (
-        <form>
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            aoEnviar();
+        }}>
             <TextField 
                 id="email" 
                 label="email" 
                 type="email" 
+                required
                 variant="outlined"
                 margin="normal"
                 fullWidth 
@@ -17,6 +21,7 @@ function DadosUsuario() {
                 id="senha" 
                 label="senha" 
                 type="password" 
+                required
                 variant="outlined"
                 margin="normal"
                 fullWidth 
